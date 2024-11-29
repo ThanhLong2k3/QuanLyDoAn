a4ac1fc224a65bb53c049d94be44fc8db8d396c1
unknown <jaykergg@gmail.com>
2024-11-29 23:54:12 +0700

    BACKEND QL_HOIDONG

---
+++ be/Back-end_QLDoAn/DAL/InterFace/QL_DoAn/IDotLmaDoAnRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO.QL_DoAn;
+
+namespace DAL.InterFace.QL_DoAn
+{
+    public partial interface IDotLmaDoAnRepository
+    {
+        public List<DotLamDoAnDTO> GetAll();
+        string Create(DotLamDoAnDTO model, string taikhoan);
+        string Update(DotLamDoAnDTO model, string taikhoan);
+        string Delete(string malop, string taikhoan);
+    }
+}
---
+++ be/Back-end_QLDoAn/DAL/QL_DoAnRepository/ThanhVien_HoiDongReponsitory.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.InterFace.QL_DoAn;
+using DTO.QL_DoAn;
+
+namespace DAL.QL_DoAnRepository
+{
+    public class ThanhVien_HoiDongReponsitory:IThem_ThanhVien_HoiDong
+    {
+        private IDatabaseHelper _dbHelper;
+
+        public ThanhVien_HoiDongReponsitory(IDatabaseHelper dbHelper)
+        {
+            _dbHelper = dbHelper;
+        }
+        public string Create(ThanhVien_HoiDongDTO model, string taikhoan)
+        {
+            string msgError = "";
+            string kq = "";
+            try
+            {
+                var result = _dbHelper.ExecuteScalarSProcedureWithTransaction(out msgError, "Them_ThanhVien_HoiDong",
+                   "@taiKhoan", taikhoan,
+                   "@maHoiDong",model.maHoiDong,
+                   "@maThanhVien",model.maThanhVien,
+                   "@loaiThanhVien",model.loaiThanhVien
+                );
+
+                if (result != null)
+                {
+                    kq = result.ToString();
+                }
+                else
+                {
+                    kq = "Không có phản hồi từ server";
+                }
+                return kq;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Đã xảy ra lỗi trong quá trình thêm lớp: " + ex.Message, ex);
+            }
+        }
+        public string Delete(ThanhVien_HoiDongDTO model, string taikhoan)
+        {
+            string msgError = "";
+            string kq = "";
+            try
+            {
+                var result = _dbHelper.ExecuteScalarSProcedure(out msgError, "Xoa_ThanhVien_HoiDong",
+                        "@taiKhoan", taikhoan,
+                   "@maHoiDong", model.maHoiDong,
+                   "@maThanhVien", model.maThanhVien,
+                   "@loaiThanhVien", model.loaiThanhVien
+                    );
+
+                if (result != null)
+                {
+                    kq = result.ToString();
+                }
+                else
+                {
+                    kq = "Không có phản hồi từ server";
+                }
+                return kq;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
